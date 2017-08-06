c97ca237cfc1a676a5790f1c9db65e25c44058de
andikayana <andikayana99@gmail.com>
2017-08-06 20:09:10 +0800

    penambahan data dummy

--- app/src/main/java/com/blikadek/news/activity/MainActivity.java
+++ app/src/main/java/com/blikadek/news/activity/MainActivity.java
@@ -2,9 +2,16 @@ package com.blikadek.news.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.blikadek.news.R;
+import com.blikadek.news.adapter.NewsAdapter;
+import com.blikadek.news.model.ArticlesItem;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,6 +19,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.recyclerView) RecyclerView mRecycleView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private NewsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+       mLinearLayoutManager = new LinearLayoutManager(
+                this,
+                LinearLayoutManager.VERTICAL,
+                false
+        );
+
+        mAdapter = new NewsAdapter(getDummyData());
+        mRecycleView.setLayoutManager(mLinearLayoutManager);
+        mRecycleView.setAdapter(mAdapter);
+
+
+
+    }
+
+    private List<ArticlesItem> getDummyData() {
+        List<ArticlesItem> dummyList = new ArrayList<>();
+
+        for (int i=0; i < 10; i ++) {
+            ArticlesItem dummyNews = new ArticlesItem();
+            dummyNews.setTitle(String.valueOf(i)+ getString(R.string.dummy_news_title));
+            dummyNews.setDescription(getString(R.string.dummy_news_desc));
+            dummyNews.setUrlToImage("https://tctechcrunch2011.files.wordpress.com/2017/08/aug_chart_1.png?w=764&h=400&crop=1");
+
+            dummyList.add(dummyNews);
+        }
+        return dummyList;
     }
 }
--- app/src/main/java/com/blikadek/news/adapter/NewsAdapter.java
+++ app/src/main/java/com/blikadek/news/adapter/NewsAdapter.java
@@ -4,8 +4,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.blikadek.news.R;
+import com.blikadek.news.model.ArticlesItem;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by M13x5aY on 06/08/2017.
@@ -13,31 +22,50 @@ import com.blikadek.news.R;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder>{
 
+    List<ArticlesItem> mNewsList;
 
-
+    public NewsAdapter(List<ArticlesItem> mNewsList) {
+        this.mNewsList = mNewsList;
+    }
 
     @Override
     public NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_news, parent);
+                .inflate(R.layout.item_news, parent, false);
         NewsViewHolder viewHolder = new NewsViewHolder(view);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(NewsViewHolder holder, int position) {
-
+        ArticlesItem news = mNewsList.get(position);
+        holder.bind(news);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mNewsList.size();
     }
 
+
+    //ViewHolder
     static class NewsViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.ivNewsPhoto) ImageView ivNewsPhoto;
+        @BindView(R.id.tvNewsTitle) TextView tvNewsTitle;
+        @BindView(R.id.tvNewsDesc) TextView tvNewsDesc;
 
         public NewsViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bind(ArticlesItem newsitem) {
+            tvNewsTitle.setText(newsitem.getTitle());
+            tvNewsDesc.setText(newsitem.getDescription());
+            Glide.with(ivNewsPhoto.getContext())
+                    .load(newsitem.getUrlToImage())
+                    .into(ivNewsPhoto);
         }
     }
 
