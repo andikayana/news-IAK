d88bae5b579304f16a731c7c8d3663432af78b59
andikayana <andikayana99@gmail.com>
2017-08-12 14:39:30 +0800

    perubahan initialisasi

--- app/src/main/java/com/blikadek/news/activity/DetailActivity.java
+++ app/src/main/java/com/blikadek/news/activity/DetailActivity.java
@@ -8,16 +8,20 @@ import android.webkit.WebViewClient;
 
 import com.blikadek.news.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class DetailActivity extends AppCompatActivity {
 
-
+    @BindView(R.id.webView) WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        WebView mWebView =(WebView)findViewById(R.id.webView) ;
+        ButterKnife.bind(this);
+        //WebView mWebView =(WebView)findViewById(R.id.webView) ;
 
        //mWebView = new WebView(this);
         String url = getIntent().getStringExtra("url");
